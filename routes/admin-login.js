f853aa64ab4284c4a3c75fe8f3a9ffce07f2e9a1
Alexander Wang <alex915979wang@gmail.com>
2020-06-26 10:35:34 -0400

    userid session will redirect login to admin page

--- routes/admin-login.js
+++ routes/admin-login.js
@@ -6,10 +6,18 @@ var router = express.Router();
  * Admin Login Page
  *
  * Page for Administrator Login
+ * 
+ * If the userid session already exists, we will redirect to the 
+ * admin page instead.
  */
 router.get("/", function(req, res, next) {
-  let renderAlert = req.query.renderAlert === "true" ? true : false;
-  res.render('admin-login', { renderAlert : renderAlert });
+  if (req.session.userid) {
+    res.redirect('/admin-page');
+  }
+  else {
+    let renderAlert = req.query.renderAlert === "true" ? true : false;
+    res.render('admin-login', { renderAlert : renderAlert });
+  }
 });
 
 module.exports = router;
